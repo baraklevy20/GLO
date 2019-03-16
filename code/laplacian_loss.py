39a4319a075835522fd480c75df4b0795ae6ecb8
barak.levy <barak.levy1@cs.huji.ac.il>
2019-03-16 16:46:25 +0200

    Finished the Laplacian Loss

--- code/laplacian_loss.py
+++ code/laplacian_loss.py
@@ -1,68 +1,75 @@
 import numpy as np
-from scipy import signal
+import torch
+import torch.nn as nn
+import torch.nn.functional as fnn
 
-def compute_kernel(kernel_size):
-    # Get the kernel by applying a convolution of [1 1] on itself [kernel_size - 1] times
-    kernel = np.array([[1]]).astype(np.float64)
-    for i in range(kernel_size - 1):
-        kernel = signal.convolve2d(kernel, np.array([[1, 1]]))
-
-    # To normalize it, we need to divide by the total sum of the kernel.
-    # The sum of the n'th row of the pascal triangle is 2^(n-1)
-    return kernel / (2 ** (kernel_size - 1))
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-def build_gaussian_pyramid(im, max_levels, filter_size):
-    kernel = compute_kernel(filter_size).astype(np.float64)
-    pyramids = []
+def compute_kernel(kernel_size, number_of_channels):
+    grid = np.float32(np.mgrid[0:kernel_size, 0:kernel_size].T)
 
-    # Go through each level
-    for i in range(max_levels):
-        # Add the current image to the pyramid
-        pyramids.append(im)
+    # Using the real gaussian instead of an approximation we saw in image processing class
+    gaussian = lambda x: np.exp(-1 / 2 * (x - kernel_size // 2) ** 2) ** 2
+    kernel = np.sum(gaussian(grid), axis=2)
 
-        # Reduce the image
-        im = signal.convolve2d(im, kernel, mode='same', boundary='symm')
-        im = signal.convolve2d(im, kernel.T, mode='same', boundary='symm')
-        im = im[::2, ::2]
+    # Normalize the kernel
+    kernel /= np.sum(kernel)
 
-        # Stop if the image gets too small
-        if im.shape[0] < 16 or im.shape[1] < 16:
-            break
+    # Repeat it for every channel
+    kernel = np.tile(kernel, (number_of_channels, 1, 1))
 
-    return pyramids, kernel
+    # Conv2D works with a kernel as a tensor
+    kernel = torch.FloatTensor(kernel[:, None, :, :])
+
+    # Convert the kernel to CPU or CUDA
+    kernel = kernel.to(device)
+
+    return kernel
 
 
-def build_laplacian_pyramid(im, max_levels, filter_size):
-    # Compute the Gaussian pyramids
-    gaussians, kernel = build_gaussian_pyramid(im, max_levels, filter_size)
+def apply_kernel(img, kernel):
+    n_channels, _, kernel_size, _ = kernel.shape
 
-    # Use it to get the Laplacian pyramids
-    laplacians = list(((gaussians[i] - expand(gaussians[i + 1], kernel)) for i in range(len(gaussians) - 1)))
+    # Convolve the image with the kernel. Add padding so that the size won't change
+    return fnn.conv2d(img, kernel, groups=n_channels, padding=kernel_size // 2)
+
+
+def build_laplacian_pyramid(im, kernel, max_levels):
+    current_gaussian = im
+    laplacian_pyramids = []
+
+    for level in range(max_levels):
+        # Calculate the Gaussian pyramid
+        next_gaussian = apply_kernel(current_gaussian, kernel)
+
+        # Calculate the Laplacian pyramid using the Gaussian
+        laplacian_pyramids.append(current_gaussian - next_gaussian)
+
+        # Move on to the next level of the pyramid
+        current_gaussian = fnn.avg_pool2d(next_gaussian, 2)
 
     # The last pyramid is the last Gaussian pyramid
-    laplacians.append(gaussians[len(gaussians) - 1])
-    return laplacians, kernel
+    laplacian_pyramids.append(current_gaussian)
+
+    return laplacian_pyramids
 
 
-def expand(im, kernel):
-    # Enlarge the image
-    new_im = np.zeros((2 * im.shape[0], 2 * im.shape[1]))
-    new_im[:: 2, :: 2] = im
+class LaplacianLoss(nn.Module):
+    def __init__(self, max_levels=5, kernel_size=5):
+        super(LaplacianLoss, self).__init__()
+        self.max_levels = max_levels
+        self.kernel_size = kernel_size
+        self.kernel = None
 
-    # Blur it
-    new_im = signal.convolve2d(new_im, 2 * kernel, mode='same', boundary='symm')
-    new_im = signal.convolve2d(new_im, 2 * kernel.T, mode='same', boundary='symm')
+    def forward(self, input, target):
+        # Compute the kernel once
+        if self.kernel is None:
+            self.kernel = compute_kernel(self.kernel_size, input.shape[1])
 
-    return new_im
+        # Calculate the Laplacian pyramids
+        source = build_laplacian_pyramid(input, self.kernel, self.max_levels)
+        target = build_laplacian_pyramid(target, self.kernel, self.max_levels)
 
-
-def laplacian_loss(im1, im2, max_level):
-    lap1 = build_laplacian_pyramid(im1, max_level, 5)
-    lap2 = build_laplacian_pyramid(im2, max_level, 5)
-
-    sum = 0
-    for j in range(max_level):
-        sum += 2 ** (- 2 * j) * abs(lap1[j] - lap2[j])
-
-    return sum
+        # Sum over the different levels
+        return sum(fnn.l1_loss(s, t) for s, t in zip(source, target))
